0705cd18070613a2823d620030c7eba7824e1716
itsmehemant7 <itsmehemant7@gmail.com>
2022-08-08 18:46:51 +0530

    cleaning dir tree structure

--- bookAPI/routes.py
+++ bookAPI/routes.py
@@ -1,142 +1,12 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-from flask_swagger_ui import get_swaggerui_blueprint
 import json
 import datetime
-
-app = Flask(__name__)
-
-# .....swagger conf.....#
-SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
-API_URL = '/static/swagger.json'  # Our API url (can of course be a local resource)
-SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': "Book Management API"
-    }
-)
-app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+from flask import request, jsonify
+from bookAPI import db, app, login_manager
+from bookAPI.models import User, Publication
+from flask_login import login_user, login_required, logout_user, current_user
 
 
-#...for authentication...
-login_manager = LoginManager()
-
-# ....getting data from config.json file.... #
-with open('config.json','r') as config:
-    fConfig= json.load(config)
-
-#...App URL config...
-assert isinstance(fConfig["BASE_URL"], str)
-assert  "http" in fConfig['BASE_URL']
-BASE_URL = fConfig["BASE_URL"]
-
-# secret App key config
-SECRET_KEY = fConfig.get('SECRET_KEY')
-assert isinstance(SECRET_KEY, str)
-assert SECRET_KEY != ""
-app.secret_key = SECRET_KEY
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# setting DB URI
-DB_URI = fConfig['PARAMS']['DB_URI']
-assert isinstance(DB_URI, str)
-assert DB_URI != ""
-app.config['SQLALCHEMY_DATABASE_URI']= DB_URI
-
-db=SQLAlchemy(app)
-login_manager.init_app(app)
-
-
-class User(db.Model):
-    """An admin user capable of viewing reports/table's data"""
-
-    __tablename__ = 'user'
-
-    email = db.Column(db.String, primary_key=True, unique=True)
-    username = db.Column(db.String)
-    image = db.Column(db.String)
-    password = db.Column(db.String)
-    authenticated = db.Column(db.Boolean, default=False)
-
-    @property
-    def serialize(self):
-       """Return object data in easily serializable format"""
-       return {
-           'email': self.email,
-           'username': self.username,
-           'image': self.image,
-           'password': self.password,
-           'authenticated': self.authenticated,
-       }
-
-    def is_active(self):
-        """True, as all users are active."""
-        return True
-
-    def get_id(self):
-        """Return the email address to satisfy Flask-Login's requirements."""
-        return self.email
-
-    def is_authenticated(self):
-        """Return True if the user is authenticated."""
-        return self.authenticated
-
-    def is_anonymous(self):
-        """False, as anonymous users aren't supported."""
-        return False
-
-
-class Publication(db.Model):
-    """
-      MODEL: model class for book publications
-    """
-
-    __tablename__= 'publication'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    description = db.Column(db.String(300), nullable=False)
-    priority = db.Column(db.String(20), nullable=False)
-    created_at = db.Column(db.String(100), nullable=False)
-    updated_at = db.Column(db.String(100), nullable=False)
-    status = db.Column(db.String(50), nullable=False)
-    user = db.Column(db.String(30), nullable=False)
-    def __init__(self, title, description, priority, created_at, status, updated_at, user):
-        self.title= title
-        self.description= description
-        self.priority= priority
-        self.created_at= created_at
-        self.status= status
-        self.updated_at= updated_at
-        self.user= user
-
-    @property
-    def serialize(self):
-       """Return object data in easily serializable format"""
-       return {
-           'id': self.id,
-           'title': self.title,
-           'description': self.description,
-           'priority': self.priority,
-           'created_at': self.created_at,
-           'status': self.status,
-           'user': self.user,
-           'updated_at': self.updated_at,
-           'time_since_published': self.life()
-       }
-
-    def life(self):
-         """Generates time elapsed by taking created date as refernce"""
-         t1 = datetime.datetime.strptime(self.created_at, '%Y-%m-%d %H:%M:%S.%f')
-         t2 = datetime.datetime.now()
-         self.time_diff = t2 - t1
-         self.time_diff_sec = self.time_diff.total_seconds()
-         return str(datetime.timedelta(seconds = self.time_diff_sec))
-
-
-db.create_all()
+# ........app routes...........
 
 
 @app.route('/', methods=['GET'])
@@ -158,8 +28,7 @@ def addBook():
   if request.method=='POST':
       data = request.get_json()
       testBook = Publication.query.filter_by(title=data.get('title')).filter_by(description=data.get('description')).first()
-      if testBook is not None:
-          return jsonify({'status':404, 'message':'This book already exist!!'})
+
       book = Publication(
           title = data.get('title'),
           description = data.get('description'),
@@ -186,15 +55,19 @@ def updatebook(id):
   """
   if request.method=='PUT':
       data = request.get_json()
-      book = Publication.query.get(id)
+      book = Publication.query.filter_by(id=id).first()
       if book is not None:
           if current_user.email == book.user:
-              book.title = data.get('title')
-              book.description = data.get('description')
-              book.priority = data.get('priority')
-              book.status = data.get('status')
+              if data.get('title'):
+                  book.title = data.get('title')
+              if data.get('description'):
+                  book.description = data.get('description')
+              if data.get('priority'):
+                  book.priority = data.get('priority')
+              if data.get('status'):
+                  book.status = data.get('status')
               book.updated_at = datetime.datetime.now()
-              db.session.add(book)
+              
               db.session.commit()
               return jsonify({'status':200, 'message':'your book is Updated!!'})
           else:
@@ -323,7 +196,7 @@ def updateDp():
         if data.get('image') is not None:
             image = data.get('image')
             filepath = image.filename
-            image.save(f"./static/uploads/images/{filepath}")
+            image.save(f"/static/uploads/images/{filepath}")
             user.image= filepath
             db.session.commit()
             return jsonify({'status':200, "message":"changes done!!"})
@@ -408,10 +281,3 @@ def logout():
     db.session.commit()
     logout_user()
     return jsonify({'status':200, 'message':'successfuly logged out!!'})
-
-
-# .......................Starting the application.................... #
-
-
-if __name__ == '__main__':
-  app.run(debug=True)
